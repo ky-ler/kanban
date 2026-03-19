7cea07833abe3b7f5c1aaaf2677770736e786247
Kyler Riggs <100742516+ky-ler@users.noreply.github.com>
2026-03-19 17:27:35 -0400

    refactor: remove unused components and hooks

--- client/src/routes/_protected/settings.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_protected/settings")({
-  component: SettingsComponent,
-});
-
-function SettingsComponent() {
-  return <div>Hello "/_protected/settings"!</div>;
-}
