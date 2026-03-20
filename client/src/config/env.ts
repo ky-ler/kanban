157c3386bcfb4fa273044649b176b1e6cbaed5fa
Kyler Riggs <100742516+ky-ler@users.noreply.github.com>
2026-03-20 02:48:00 -0400

    fix: move VITE_OPENAPI_YAML_URL to non-production environment configuration

--- client/src/config/env.ts
+++ client/src/config/env.ts
@@ -2,11 +2,19 @@ import { z } from "zod";
 
 const EnvSchema = z.object({
   VITE_API_URL: z.string().url(),
-  VITE_OPENAPI_YAML_URL: z.string().url(),
   VITE_AUTH0_DOMAIN: z.string(),
   VITE_AUTH0_CLIENT_ID: z.string(),
   VITE_AUTH0_AUDIENCE: z.string(),
   VITE_AUTH0_CALLBACK_URL: z.string().url(),
 });
+
+if (import.meta.env.MODE !== "production") {
+  EnvSchema.extend({
+    VITE_OPENAPI_YAML_URL: z
+      .string()
+      .url()
+      .default("http://localhost:8080/api-docs.yaml"),
+  });
+}
 
 export const env = EnvSchema.parse(import.meta.env);
